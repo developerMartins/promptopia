762aaf65a27ef15ad1c080f857ea886477478767
imartinsCodes <martssilvapro@gmail.com>
2023-08-28 11:55:12 -0300

    database config implemented

---
+++ utils/database.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if(isConnected) {
+    console.log('Connected to Mongo!');
+    return
+  };
+
+  try {
+    await mongoose.connect(process.env.MONGOBD_URI, {
+      dbName: 'share_prompt',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+
+    console.log('MongoDB connected!');
+  } catch (error) {
+    console.log(error);
+  };
+
+};
