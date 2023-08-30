91c117b5617ccacd155494f46485e4fbac47c54a
imartinsCodes <martssilvapro@gmail.com>
2023-08-30 10:44:36 -0300

    CreatePrompt page implemented

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,32 @@
+'use client';
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
+
+  const [submitting, setSubmitting] = useState(false);
+
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: '',
+  });
+
+  const createPrompt = async (e) => {
+
+  }
+
   return (
-    <div>CreatePrompt</div>
+    <Form
+      type="Create"
+      post={ post }
+      setPost={ setPost }
+      submitting={ submitting }
+      handleSubmit={ createPrompt }
+    />
   );
 };
 
