2e6216d350d615b167e6115b8260806202369a7c
imartinsCodes <martssilvapro@gmail.com>
2023-09-26 10:30:32 -0300

    basic seacrh input addded

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -5,8 +5,26 @@ import { useEffect, useState } from "react";
 import PromptCard from "./PromptCard";
 
 const Feed = () => {
+
+  const [searchText, setSearchText] = useState('');
+
+  const hendleSearchChange = () => {
+
+  };
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or an username"
+          value={ searchText }
+          onChange={ hendleSearchChange }
+          required
+          className="search_input peer"
+        />
+      </form>
+    </section>
   );
 };
 
