385dbedb5c5ef7025834c4178416d7705178850f
imartinsCodes <martssilvapro@gmail.com>
2023-10-05 15:28:09 -0300

    delete & edit buttons added

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,7 +9,11 @@ import { useState } from "react";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
+  const { data: session } = useSession();
   const [copied, setCopied] = useState("");
+
+  const pathName = usePathname();
+  const router = useRouter();
 
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -63,6 +67,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         { post.tag }
       </p>
+
+      { session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p 
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={ handleEdit }
+          >
+            Edit
+          </p>
+          <p 
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={ handleDelete }
+          >
+            Delete
+          </p>
+        </div>
+      ) }
     </div>
   );
 };
