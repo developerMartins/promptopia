faf2a7fac72281dca4927d60f1d7098eb413d875
imartinsCodes <martssilvapro@gmail.com>
2023-10-05 14:46:22 -0300

    basic profile page implemented

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,8 +1,46 @@
+"use client"
 
-const page = () => {
+import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+
+    const { data: session } = useSession();
+
+    const fetchPosts = async () => {
+      const res = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await res.json();
+
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  const handleEdit = () => {
+
+  };
+
+  const handleDelete = async () => {
+
+  };
+
   return (
-    <div>page</div>
+    <Profile
+      name="My"
+      desc="Wellcome to your personalized profile page"
+      data={ posts }
+      handleEdit={ handleEdit }
+      handleDelete={ handleDelete }
+    />
   );
 };
 
-export default page;
+export default MyProfile;
